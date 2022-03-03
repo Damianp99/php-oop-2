d1f7fce575172bd097075505d823b610c6094172
Damianp99 <damianperez200199@gmail.com>
2022-03-03 17:22:18 +0100

    class customer

---
+++ class/Customer.php
@@ -0,0 +1,69 @@
+<?php
+class Customer
+{
+    public $first_name;
+    public $last_name;
+    public $age;
+    public $card;
+    public $registered;
+    public function __construct($first_name, $last_name, $age, $card, $registered = null)
+    {
+        $this->setFirstName($first_name);
+        $this->setLastName($last_name);
+        $this->setAge($age);
+        $this->setCard($card);
+        $this->setRegistered($registered);
+    }
+    // FIRST NAME
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+    public function setFirstName($first_name)
+    {
+        $this->first_name = $first_name;
+    }
+
+    // LAST NAME
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName($last_name)
+    {
+        $this->last_name = $last_name;
+    }
+
+    // AGE
+    public function getAge()
+    {
+        return $this->first_name;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    // CARD 
+    public function getCard()
+    {
+        return $this->card;
+    }
+    public function SetCard($card)
+    {
+        $this->card = $card;
+    }
+
+    // REGISTERED
+    public function setRegistered($registered)
+    {
+        if (!$registered) return false;
+        return true;
+    }
+}
+
+$person = new Customer('Pippo', 'Franco', '54', '1000',);
+
+var_dump($person);
